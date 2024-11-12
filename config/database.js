4b720132ae106d0808b3eff0d8aa3decf191d68c
ducvu <vu99550@donga.edu.vn>
2024-11-12 09:20:10 +0700

    update config mongodb

--- config/database.js
+++ config/database.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/laptrinhdidong', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://mongodb:27017/laptrinhdidong', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+
     .then(() => console.log('Kết nối đến MongoDB thành công!'))
     .catch((err) => {
         console.error('Kết nối đến MongoDB thất bại!', err);
