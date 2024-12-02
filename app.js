e10fe3673104a7e39bcce43c625ee05642a928af
ducvu <vu99550@donga.edu.vn>
2024-12-02 17:46:43 +0700

    delete auth

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const cors = require('cors');
 const connectDB = require('./config/db');
 const shoeRoutes = require('./routes/shoeRoutes');
 const dotenv = require('dotenv');
 
 dotenv.config();
 const app = express();
-app.use(cors());
 app.use(express.json());
 
 // Kết nối đến MongoDB
