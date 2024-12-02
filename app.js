a0954db1de32bb43ce9c04f86866786454953388
ducvu <vu99550@donga.edu.vn>
2024-12-02 18:29:04 +0700

    update

--- app.js
+++ app.js
@@ -5,17 +5,25 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 // Kết nối đến MongoDB
 connectDB();
 
+// Cấu hình header CORS thủ công
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // Cho phép frontend từ localhost:3000
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 // Sử dụng các routes
 app.use('/api/shoes', shoeRoutes);
 
-
 // Khởi động server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
