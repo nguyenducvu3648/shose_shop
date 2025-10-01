a0342654d1fb235fe03c4e5366d0dbf36d0fed5b
ducvu <vu99550@donga.edu.vn>
2025-10-01 18:33:43 +0700

    update app

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ app.use(express.json());
 // Kết nối đến MongoDB
 connectDB();
 
-// Cấu hình header CORS
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
